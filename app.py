11a007b438c9976703aa325938ebb8a283c26a71
Илья Крутилин <Laplak99@mail.ru>
2022-05-01 18:22:25 +0300

    added telegram bot

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from data import db_session
 from data.films import Film
 from data.questions import Question
 from data.question_adder import add_question
+from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import CallbackContext, CommandHandler
+from telegram.ext import CommandHandler
+from telegram import ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup, Update
+import logging
+from telegram import ReplyKeyboardRemove
 
 
 db_session.global_init("db/films.db")
@@ -70,3 +76,114 @@ with open('questions.txt', mode='r') as file:
 
         add_question(line_elements[1], complexity, film,
                      answers[0], answers[1], answers[2], answers[0])
+
+
+# Запускаем логгирование
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+
+TOKEN = '5106957189:AAFCPjSNNQWtuLtmYp62v9kdmU5hoZQnaRk'
+
+reply_keyboard = [['Пираты Карибского моря'], ['Острые козырьки'], ['Один дома'], ['Бриллиантовая рука'], ['Джентльмены']]
+
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+
+
+def close_keyboard(update, context):
+    update.message.reply_text(
+        "Ok",
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+# Напишем соответствующие функции.
+# Их сигнатура и поведение аналогичны обработчикам текстовых сообщений.
+def start(update, context):
+    username = update.message.from_user.username
+    update.message.reply_text(
+        f"Добро пожаловать, {username}!\nЯ - TestFilmBot - бот, при помощи которого вы можете проходить тесты на знание фильмов.\nВыберите фильм и уровень сложности, чтобы начать.",
+    reply_markup=markup
+    )
+
+
+def stop(update, context):
+    username = update.message.from_user.username
+    update.message.reply_text(
+        f"До встречи, {username}!\nНадеюсь, вам понравилось проходить тесты!")
+
+def help(update, context):
+    update.message.reply_text(
+        "Я пока не умею помогать... Я только ваше эхо.")
+
+def pirates_of_the_Caribbean(update, context):
+    update.message.reply_text(
+        "asd")
+
+def peaky_Blinders(update, context):
+    update.message.reply_text(
+        "bebra")
+
+
+def home_Alone(update, context):
+    update.message.reply_text(
+        "")
+
+
+def the_diamond_arm(update, context):
+    update.message.reply_text("")
+
+
+def the_Gentlemen(update, context):
+    update.message.reply_text(
+        "")
+
+
+
+
+# Определяем функцию-обработчик сообщений.
+# У неё два параметра, сам бот и класс updater, принявший сообщение.
+
+
+def main():
+    # Создаём объект updater.
+    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
+    updater = Updater(TOKEN)
+
+    # Получаем из него диспетчер сообщений.
+    dp = updater.dispatcher
+
+    #keyboard
+
+
+
+
+    # Создаём обработчик сообщений типа Filters.text
+    # После регистрации обработчика в диспетчере
+    # эта функция будет вызываться при получении сообщения
+    # с типом "текст", т. е. текстовых сообщений.
+
+    # Регистрируем обработчик в диспетчере.
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("close", close_keyboard))
+
+    dp.add_handler(MessageHandler(Filters.text, pirates_of_the_Caribbean))
+    dp.add_handler(MessageHandler(Filters.text, peaky_Blinders))
+   # dp.add_handler(MessageHandler("home_Alone", home_Alone))
+   # dp.add_handler(MessageHandler("the_diamond_arm", the_diamond_arm))
+   # dp.add_handler(MessageHandler("the_Gentlemen", the_Gentlemen))
+
+    # Запускаем цикл приема и обработки сообщений.
+    updater.start_polling()
+
+    # Ждём завершения приложения.
+    # (например, получения сигнала SIG_TERM при нажатии клавиш Ctrl+C)
+    updater.idle()
+
+
+# Запускаем функцию main() в случае запуска скрипта.
+if __name__ == '__main__':
+    main()
